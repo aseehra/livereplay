f6ae1f5cdc7ef6057bef5f9f5b7295b556846e47
Arun Seehra <seehra.a@gmail.com>
2014-08-03 02:05:43 -0500

    Add empty XCode project.

---
+++ ios/InCaseYouMissedIt/InCaseYouMissedIt/ICYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ICYAppDelegate.h
+//  InCaseYouMissedIt
+//
+//  Created by Arun Seehra on 8/3/14.
+//  Copyright (c) 2014 Arun Seehra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ICYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/InCaseYouMissedIt/InCaseYouMissedIt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InCaseYouMissedIt
+//
+//  Created by Arun Seehra on 8/3/14.
+//  Copyright (c) 2014 Arun Seehra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ICYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([ICYAppDelegate class]));
+  }
+}
